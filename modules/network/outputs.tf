76e28ee8908974a1fc3ce03fbf9ded8436b4fbef
MShabade <mpshabade@gmail.com>
2026-03-15 17:52:51 +0000

    updated output as per module

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,19 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "public_subnet_ids" {
-  value = [for s in aws_subnet.public : s.id]
+  description = "VPC ID"
+  value       = aws_vpc.this.id
 }
 
 output "private_subnet_ids" {
-  value = [for s in aws_subnet.private : s.id]
+  description = "List of private subnet IDs"
+  value       = [for s in aws_subnet.private : s.id]
 }
 
-output "nat_gateway_ids" {
-  value = [for ng in aws_nat_gateway.this : ng.id]
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.this.id
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [for s in aws_subnet.public : s.id]
 }
